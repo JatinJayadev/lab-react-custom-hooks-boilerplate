dce1f86ae2e63b3a6943f08feee5ee3681995e97
Jatin Jayadev <jatin.batchu@kalvium.community>
2024-02-28 21:51:41 +0530

    Done

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import useStorage from './Hook/useStorage'
+function App() {
+  let { setStorage } = useStorage()
+
+  return (
+    <>
+      <h2>Enter Some Text</h2>
+      <input type="text" placeholder='Enter Some Text' style={{ padding: " 5px 10px" }} onChange={(e) => setStorage(e.target.value)} />
+    </>
+  )
+}
+
+export default App;
