8d577af5ca5215f825ca6197b17859cc07b2365c
Jatin Jayadev <jatin.batchu@kalvium.community>
2024-02-28 21:51:10 +0530

    Done!

---
+++ src/Hook/useStorage.jsx
@@ -0,0 +1,9 @@
+const useStorage = () => {
+    let setStorage = (val) => {
+        localStorage.setItem("data", val)
+        sessionStorage.setItem("data", val)
+    }
+    return { setStorage }
+}
+
+export default useStorage;
